82d7eebff006f829845ee403bed07fd356aa88ba
matsumo-and <yh134.toisanda@gmail.com>
2022-06-23 01:37:20 +0900

    formatter

--- Java-CUI-CannotStop/src/cannnotStop/Dice.java
+++ Java-CUI-CannotStop/src/cannnotStop/Dice.java
@@ -6,9 +6,10 @@ public class Dice {
 	final int MIN = 1;
 	final int MAX = 6;
 	final Random random = new Random();
-	
-	public Dice() {}
-	
+
+	public Dice() {
+	}
+
 	public int roll() {
 		//num = random number from MIN to MAX
 		int num = random.nextInt(MAX - MIN + 1) + MIN;
--- Java-CUI-CannotStop/src/cannnotStop/Play.java
+++ Java-CUI-CannotStop/src/cannnotStop/Play.java
@@ -5,7 +5,7 @@ public class Play {
 	public static void main(String[] args) {
 		Dice dice = new Dice();
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 15; i++) {
 			System.out.println(dice.roll());
 		}
 	}
