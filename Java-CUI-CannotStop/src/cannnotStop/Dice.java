c9b65fe2667798759dc884ec781d5733282999cb
matsumo-and <yh134.toisanda@gmail.com>
2022-06-23 01:23:39 +0900

    Create Dice

---
+++ Java-CUI-CannotStop/src/cannnotStop/Dice.java
@@ -0,0 +1,14 @@
+package cannnotStop;
+
+import java.util.Random;
+
+public class Dice {
+	final int MIN = 1;
+	final int MAX = 6;
+	
+	final Random random = new Random();
+
+	public int roll() {
+		return 1;
+	}
+}
