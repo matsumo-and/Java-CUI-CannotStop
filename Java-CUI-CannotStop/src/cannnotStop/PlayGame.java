7c8c16afcb5763a5115bbe57feea7e604d461e42
matsumo-and <yh134.toisanda@gmail.com>
2022-06-25 23:47:42 +0900

    option with userChoise

--- Java-CUI-CannotStop/src/cannnotStop/PlayGame.java
+++ Java-CUI-CannotStop/src/cannnotStop/PlayGame.java
@@ -3,15 +3,13 @@ package cannnotStop;
 public class PlayGame {
 
 	public static void main(String[] args) {
-
-		//int i = PlayerMethod.getChoise(0,1);
-
 		Player user = new Player();
-		
+
 		for (int i = 1; i <= 10; i++) {
 			Integer[][] optionList = PlayerMethod.getCombination();
-
-			user.setLane(optionList[1][1]);
+			int userChoise = PlayerMethod.getChoise(0, 2);
+			user.setLane(optionList[userChoise][0]);
+			user.setLane(optionList[userChoise][1]);
 		}
 	}
 
